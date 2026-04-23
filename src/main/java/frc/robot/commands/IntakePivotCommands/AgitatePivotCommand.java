95842d78180e9edef607152b351c3483ebfb1bdd
DylanJakeComilang <dylanjakecomilang@gmail.com>
2026-04-22 14:48:16 -1000

    cleanup intake-pivot commands

--- src/main/java/frc/robot/commands/IntakePivotCommands/AgitatePivotCommand.java
+++ src/main/java/frc/robot/commands/IntakePivotCommands/AgitatePivotCommand.java
@@ -1,7 +1,3 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.commands.IntakePivotCommands;
 
 import org.ejml.equation.IntegerSequence.Range;
@@ -11,7 +7,6 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.PivotSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
 
-/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class AgitatePivotCommand extends Command {
   IntakeSubsystem intake;
   PivotSubsystem pivot;
@@ -19,17 +14,15 @@ public class AgitatePivotCommand extends Command {
   double pivotOut = 28;
   boolean MovingInOrOut;
   Timer timer = new Timer();
-  /** Creates a new AgitatorCommand. */
+
   public AgitatePivotCommand(PivotSubsystem newPivotSubsystem, IntakeSubsystem newintakeSubsystem) {
     intake = newintakeSubsystem;
     pivot = newPivotSubsystem;
     addRequirements(pivot);
     addRequirements(intake);
-    // Use addRequirements() here to declare subsystem dependencies.
+
   }
 
-
-  // Called when the command is initially scheduled.
   @Override
   public void initialize() { 
     MovingInOrOut = false;
@@ -37,15 +30,8 @@ public class AgitatePivotCommand extends Command {
     intake.setSpeed(0.35);
   }
 
-  // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    // pivot.setSetPoint(10.5);
-    // if(timer.get() > 2) {
-    //   pivot.setSetPoint(15);
-    // }
-    // timer.stop();
-    // intake.setSpeed(-0.55);
     double currentPos = pivot.getPivotEncoder();
 
     if (MovingInOrOut && currentPos >= pivotIn + 0.2) {
@@ -61,14 +47,12 @@ public class AgitatePivotCommand extends Command {
     }
   }
 
-  // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
     pivot.setSetPoint(pivotIn);
     intake.setSpeed(0);
   }
 
-  // Returns true when the command should end.
   @Override
   public boolean isFinished() {
     return false;
--- src/main/java/frc/robot/commands/IntakePivotCommands/MoveToPositionMagicCommand.java
+++ src/main/java/frc/robot/commands/IntakePivotCommands/MoveToPositionMagicCommand.java
@@ -1,7 +1,3 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-//yippie
 package frc.robot.commands.IntakePivotCommands;
 
 import edu.wpi.first.math.MathUtil;
@@ -9,37 +5,31 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.PivotSubsystem;
 
-/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class MoveToPositionMagicCommand extends Command {
   PivotSubsystem pivotMagic;
   double setpoint;
   double tolerance;
-  /** Creates a new MoveToPositionMagicCommand. */
+
   public MoveToPositionMagicCommand(PivotSubsystem newPivotIntakeMagic, double newSetpoint, double newTolerance) {
     setpoint = newSetpoint;
     tolerance = newTolerance;
     pivotMagic = newPivotIntakeMagic;
     addRequirements(pivotMagic);
-    // Use addRequirements() here to declare subsystem dependencies.
   }
 
-  // Called when the command is initially scheduled.
   @Override
   public void initialize() {}
 
-  // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
     pivotMagic.setSetPoint(setpoint);
   }
 
-  // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
     SmartDashboard.putString("isFinished", "yes");
   }
 
-  // Returns true when the command should end.
   @Override
   public boolean isFinished() {
     return MathUtil.isNear(setpoint, pivotMagic.getPivotEncoder(), tolerance);
