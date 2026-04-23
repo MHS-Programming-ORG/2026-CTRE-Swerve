7fbd72350894409a9734e5ed7d1223af4c642552
DylanJakeComilang <dylanjakecomilang@gmail.com>
2026-04-22 14:50:02 -1000

    cleaned intake roller commands

--- src/main/java/frc/robot/commands/IntakeRollerCommands/runOuttakeCommand.java
+++ src/main/java/frc/robot/commands/IntakeRollerCommands/runOuttakeCommand.java
@@ -1,7 +1,3 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.commands.IntakeRollerCommands;
 
 import edu.wpi.first.wpilibj2.command.Command;
@@ -9,7 +5,6 @@ import frc.robot.subsystems.ConveyorSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.PivotSubsystem;
 
-/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class runOuttakeCommand extends Command {
 IntakeSubsystem Outtake;
 PivotSubsystem pivot;
@@ -30,21 +25,18 @@ ConveyorSubsystem conveyor;
     pivot.setSetPoint(28);
   }
 
-  // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
     Outtake.setSpeed(-0.45);
     conveyor.setConveyorSpeed(-0.1);
   }
 
-  // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
      Outtake.setSpeed(0);
      conveyor.setConveyorSpeed(0);
   }
 
-  // Returns true when the command should end.
   @Override
   public boolean isFinished() {
     return false;
