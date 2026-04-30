24f4406c4307a2995908c8c0b2f38104045b95ef
DylanJakeComilang <dylanjakecomilang@gmail.com>
2026-04-30 12:02:42 -1000

    made kicker on calc constant 80%

--- src/main/java/frc/robot/subsystems/ShooterCalcV2.java
+++ src/main/java/frc/robot/subsystems/ShooterCalcV2.java
@@ -25,36 +25,36 @@ public class ShooterCalcV2 {
 
     private final double[][] shooterData = {
         {10, 0},
-        {25 - 4, (12 * 1) * INCH_TO_METER},//These are caution  NOT ACCURATE just a guess
-        {25 - 4, (12 * 2) * INCH_TO_METER},//These are caution NOT ACCURATE just a guess
-        {35 - 4, (12 * 3) * INCH_TO_METER},
-        {39 - 4, (12 * 4) * INCH_TO_METER},
-        {43 - 8, (12 * 5) * INCH_TO_METER},
-        {48 - 9, (12 * 6) * INCH_TO_METER},
-        {49 - 9, (12 * 7) * INCH_TO_METER},
-        {50 - 9, (12 * 8) * INCH_TO_METER},
-        {55 - 13, (12 * 9) * INCH_TO_METER},
-        {73 - 13, (12 * 10) * INCH_TO_METER},
-        {73 - 13, (12 * 11) * INCH_TO_METER}, //These are caution  NOT ACCURATE just a guess
-        {73.5 - 13, (12 * 12) * INCH_TO_METER},
-        {80 - 13, (12 * 15) * INCH_TO_METER},
+        {25, (12 * 1) * INCH_TO_METER},//These are caution  NOT ACCURATE just a guess
+        {25, (12 * 2) * INCH_TO_METER},//These are caution NOT ACCURATE just a guess
+        {35, (12 * 3) * INCH_TO_METER},
+        {39, (12 * 4) * INCH_TO_METER},
+        {43, (12 * 5) * INCH_TO_METER},
+        {48, (12 * 6) * INCH_TO_METER},
+        {49, (12 * 7) * INCH_TO_METER},
+        {50, (12 * 8) * INCH_TO_METER},
+        {55, (12 * 9) * INCH_TO_METER},
+        {73, (12 * 10) * INCH_TO_METER},
+        {73, (12 * 11) * INCH_TO_METER}, //These are caution  NOT ACCURATE just a guess
+        {73.5, (12 * 12) * INCH_TO_METER},
+        {80, (12 * 15) * INCH_TO_METER},
     };
 
     private final double[][] kickerData = {
-        {10, 0},
-        {85, (12 * 1) * INCH_TO_METER},//These are caution  NOT ACCURATE just a guess
-        {85, (12 * 2) * INCH_TO_METER},//These are caution NOT ACCURATE just a guess
-        {80, (12 * 3) * INCH_TO_METER},
-        {70, (12 * 4) * INCH_TO_METER},
-        {65, (12 * 5) * INCH_TO_METER},
-        {60, (12 * 6) * INCH_TO_METER},
-        {55, (12 * 7) * INCH_TO_METER},
-        {55, (12 * 8) * INCH_TO_METER},
-        {55, (12 * 9) * INCH_TO_METER},
-        {55, (12 * 10) * INCH_TO_METER},
-        {55, (12 * 11) * INCH_TO_METER}, //These are caution  NOT ACCURATE just a guess
-        {55, (12 * 12) * INCH_TO_METER},
-        {55, (12 * 15) * INCH_TO_METER},
+        {25*0.8, 0},
+        {25*0.8, (12 * 1) * INCH_TO_METER},//These are caution  NOT ACCURATE just a guess
+        {25*0.8, (12 * 2) * INCH_TO_METER},//These are caution NOT ACCURATE just a guess
+        {25*0.8, (12 * 3) * INCH_TO_METER},
+        {25*0.8, (12 * 4) * INCH_TO_METER},
+        {25*0.8, (12 * 5) * INCH_TO_METER},
+        {25*0.8, (12 * 6) * INCH_TO_METER},
+        {25*0.8, (12 * 7) * INCH_TO_METER},
+        {25*0.8, (12 * 8) * INCH_TO_METER},
+        {25*0.8, (12 * 9) * INCH_TO_METER},
+        {25*0.8, (12 * 10) * INCH_TO_METER},
+        {25*0.8, (12 * 11) * INCH_TO_METER}, //These are caution  NOT ACCURATE just a guess
+        {25*0.8, (12 * 12) * INCH_TO_METER},
+        {25*0.8, (12 * 15) * INCH_TO_METER},
     };
 
 
