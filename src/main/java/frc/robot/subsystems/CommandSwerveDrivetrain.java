f7e29da33107c1265d0310e16ff55c7d6ea8c88f
DylanJakeComilang <dylanjakecomilang@gmail.com>
2026-04-30 12:25:50 -1000

    so now there is feet and meters for easier tuning

--- src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
+++ src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
@@ -367,7 +367,8 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
         // SmartDashboard.putNumber("xDifference", xDifference);
         // SmartDashboard.putNumber("yDifference", yDifference);
         // SmartDashboard.putNumber("angleDifference", angleDifference);
-        SmartDashboard.putNumber("distanceDifference", distance*3.28);
+        SmartDashboard.putNumber("distDifferenceFeet", distance*3.28);
+        SmartDashboard.putNumber("distDifferenceMeter", distance);
 
         // SmartDashboard.putString("Module States",  getState().ModuleStates.toString());
 
